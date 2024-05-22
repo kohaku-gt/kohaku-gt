e435ace828867e702b387bef5e7cff78ae3afb68
bLack Domon <74413840+bLack-Domon@users.noreply.github.com>
2024-05-22 10:53:31 +0700

    Create test-yolo.js

---
+++ test-yolo.js
@@ -0,0 +1,31 @@
+function showAlert() {
+            alert("Ini adalah pesan alert!");
+        }
+
+        function showPrompt() {
+            var userInput = prompt("Masukkan nama Anda:");
+            alert("Halo, " + userInput + "!");
+        }
+
+        function showPromptWithDefault() {
+            var favoriteColor = prompt("Apa warna favorit Anda?", "Biru");
+            alert("Warna favorit Anda adalah " + favoriteColor + "!");
+        }
+
+        function showPromptWithValidation() {
+            var userAge = prompt("Berapa usia Anda?");
+            if (isNaN(userAge)) {
+                alert("Harap masukkan angka yang valid.");
+            } else {
+                alert("Usia Anda adalah " + userAge + " tahun.");
+            }
+        }
+
+        function showConfirm() {
+            var confirmAction = confirm("Apakah Anda yakin ingin melanjutkan?");
+            if (confirmAction) {
+                alert("Anda memilih untuk melanjutkan.");
+            } else {
+                alert("Anda memilih untuk membatalkan.");
+            }
+        }
